ab8e163fac9c286059fef85d670389486ecbb07a
enterth3r4in <believe.the.rain@gmail.com>
2015-11-17 13:36:47 -0500

    Completed Program

--- Chapter9Methods/Chapter9Methods/Chapter9Methods.py
+++ Chapter9Methods/Chapter9Methods/Chapter9Methods.py
@@ -4,11 +4,19 @@ def sayHello():
 
 def oneArgument(var1):
     print("Hello, ", var1, "!")
-    pass
+
+def twoArgument(var1, var2):
+    print("Hello, ", var1, "! Your color is ", var2)
+
+def valueReturned(var1, var2):
+    return var1 + " has a " + var2
 
 #main
 print("Main Started")
 sayHello()
-userName = input("Please enter your name: ")
-oneArgument(userName)
+userName = input("Please enter your name: ").strip()
+color = input("Please pick your favorite color: ").strip()
+twoArgument(userName, color)
+userAnimal = input("Enter your favorite Animal: ")
+print(valueReturned(userName, userAnimal))
 print("Main Ended")
